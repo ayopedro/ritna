dab19d5c078f14491ec2193fa426cd1aa5ed5081
Ayotunde <ayopedro2012@gmail.com>
2026-01-23 12:17:39 +0000

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 export default function Home() {
   return (
-    <div className=''>
-      <main className=''>
-        <h1 className='font-mono text-4xl font-bold text-center'>
-          Welcome to RITNA!
-        </h1>
-        <button className="btn btn-primary">Click Me</button>
-      </main>
-    </div>
+    <main className="hero">
+      <div className="flex flex-col items-center justify-center gap-8">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-8xl font-bold">Rumbles In The New Academy</h1>
+          <p className="tracking-[20px] font-semibold">RITNA</p>
+        </div>
+        <p className="text-xl">A story of chaos and travails from the mad house</p>
+        <button className="btn btn-secondary">Join Waitlist</button>
+      </div>
+    </main>
   );
 }
