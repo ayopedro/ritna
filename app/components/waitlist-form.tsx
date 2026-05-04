7b916755729732a8a0449eedd86647874f9cad7c
Ayotunde <ayopedro2012@gmail.com>
2026-05-04 23:31:21 +0100

    fix: correct file path

--- app/components/waitlist-form.tsx
+++ app/components/waitlist-form.tsx
@@ -6,8 +6,8 @@ import { SubmitHandler, useForm, Controller } from "react-hook-form";
 import { Button } from "./button";
 import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
-import { useJoinWaitlistMutation } from "../services/mutations/wishlist";
 import toast from "react-hot-toast";
+import { useJoinWaitlistMutation } from "../services/mutations/waitlist";
 
 export type JoinWaitlistFormData = {
   firstName: string;
@@ -38,7 +38,7 @@ const WaitlistForm = () => {
 
   const { mutate, isPending } = useJoinWaitlistMutation();
 
-  const submitForm = async (formData: JoinWaitlistFormData) => {
+  const submitForm: SubmitHandler<JoinWaitlistFormData> = async (formData) => {
     mutate(formData, {
       onSuccess() {
         toast.success("Successfully added to the waitlist");
@@ -109,8 +109,8 @@ const WaitlistForm = () => {
             render={({ field: { onChange, value } }) => (
               <PhoneInput
                 id="phone"
-                placeholder="8021234567"
-                value={value as any}
+                placeholder="802 123 4567"
+                value={value as string}
                 onChange={onChange}
                 defaultCountry="NG"
                 className="form-input focus-within:ring-2 focus-within:ring-blue-500 focus-within:border-blue-500"
